c56044df5071281b0b879eb5b1f14ea81c0283a6
Forgot-Dream <1360169078@qq.com>
2023-04-01 12:40:14 +0800

    feat: 支持拖拽文件到窗口. 版本更新为v1.0.2

--- src/MainView.xaml.cs
+++ src/MainView.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using ImTools;
+using STS_Bcut.src;
+using STS_Bcut.src.Common;
 
 namespace STS_Bcut
 {
@@ -7,9 +15,37 @@ namespace STS_Bcut
     /// </summary>
     public partial class MainView : Window
     {
+        private MainViewModel viewModel;
         public MainView()
         {
             InitializeComponent();
+            viewModel = (MainViewModel?)DataContext;
+        }
+
+        private void File_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                var Files = new List<string>((IEnumerable<string>)e.Data.GetData(DataFormats.FileDrop));
+                foreach (var file in Files)
+                {
+                    viewModel.Files.Add(new AudioFile()
+                    {
+                        FullName = Path.GetFileName(file),
+                        FullPath = file,
+                        IsSelected = false
+                    });
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+            }
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Link : DragDropEffects.None;
         }
     }
 }
